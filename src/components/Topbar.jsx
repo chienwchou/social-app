39658f8d44f40abd3c272ea50bd64207415b654c
Chien Wei Chou <chien.w.chou@gmail.com>
2024-03-04 12:40:38 -0800

    style topbar component

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,36 +1,37 @@
 import { Person, Search, Chat, Notifications } from "@mui/icons-material";
+import "./topbar.css";
 
 export default function Topbar() {
   return (
-    <div className="topbarContainer">
-      <div className="topbarLeft">
-        <span className="logo">Lama Social</span>
+    <div className="topbarContainer flex justify-between px-10 py-5 bg-blue-500">
+      <div className="topbarLeft font-bold text-xl basis-3/12">
+        <span className="logo text-white">Social App</span>
       </div>
-      <div className="topbarCenter">
-        <div className="searchbar">
+      <div className="topbarCenter basis-5/12">
+        <div className="searchbar flex flex-row bg-white p-1 rounded-full">
           <Search />
           <input
             placeholder="Search for friend, post or video"
-            className="searchInput"
+            className="searchInput w-full bg-transparent outline-none border-none"
           />
           <i className="searchIcon fas fa-search"></i>
         </div>
       </div>
-      <div className="topbarRight">
-        <div className="topbarLinks">
+      <div className="topbarRight flex px-5 basis-4/12 text-white items-center">
+        <div className="topbarLinks basis-4/12 flex gap-10">
           <span className="topbarLink">Homepage</span>
           <span className="topbarLink">Timeline</span>
         </div>
-        <div className="topbarIcons">
-          <div className="topbarIconItem">
+        <div className="topbarIcons flex basis-8/12 pl-5 justify-end gap-5">
+          <div className="topbarIconItem relative">
             <Person />
             <span className="topbarIconBadge">1</span>
           </div>
-          <div className="topbarIconItem">
+          <div className="topbarIconItem relative">
             <Chat />
-            <span className="topbarIconBadge">1</span>
+            <span className="topbarIconBadge">2</span>
           </div>
-          <div className="topbarIconItem">
+          <div className="topbarIconItem relative">
             <Notifications />
             <span className="topbarIconBadge">1</span>
           </div>
