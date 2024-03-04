52745adb32f12d3595e3ead44809d6883372b67d
Chien Wei Chou <chien.w.chou@gmail.com>
2024-03-04 11:30:54 -0800

    add home components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
+  return <Home />;
 }
 
 export default App;
