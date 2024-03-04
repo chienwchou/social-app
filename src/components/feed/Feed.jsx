ae8e24ff89b64a4c4be3d201f8a94b7f266a180e
Chien Wei Chou <chien.w.chou@gmail.com>
2024-03-04 14:47:09 -0800

    adding content components

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,9 @@
+import "./feed.css";
+
+export default function Feed(props) {
+  return (
+    <div className="feed">
+      <div className="feedWrapper">Feed</div>
+    </div>
+  );
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,17 @@
-import Topbar from "../../components/Topbar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Topbar from "../../components/topbar/Topbar";
 
 export default function Home() {
-  return <Topbar />;
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer flex">
+        <Sidebar className="basis-3" />
+        <Feed className="basis-5" />
+        <Rightbar className="basis-4" />
+      </div>
+    </>
+  );
 }
