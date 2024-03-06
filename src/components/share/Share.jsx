387230ce962de30ce55167ce0685ecc69b08f301
Chien Wei Chou <chien.w.chou@gmail.com>
2024-03-06 14:54:58 -0800

    adding share styles

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -3,33 +3,39 @@ import "./share.css";
 
 export default function Share() {
   return (
-    <div className="share">
-      <div className="shareWrapper">
-        <div className="shareTop">
-          <img className="shareProfileImg" src="/assets/person/1.jpeg" alt="" />
+    <div className="share m-4 shadow-xl rounded-md">
+      <div className="shareWrapper flex flex-col mx-4 my-8">
+        <div className="shareTop flex items-center py-4">
+          <img className="shareProfileImg" src="/assets/person/1.jpg" alt="" />
           <input placeholder="What's in your mind?" className="shareInput" />
         </div>
         <hr className="shareHr" />
-        <div className="shareBottom">
-          <div className="shareOptions">
-            <div className="shareOption">
-              <PermMedia />
+        <div className="shareBottom py-4 px-4">
+          <div className="shareOptions flex gap-4">
+            <div className="shareOption flex items-center gap-2">
+              <PermMedia htmlColor="tomato" />
               <span className="shareOptionText">Photo or Video</span>
             </div>
-            <div className="shareOption">
-              <Label />
+            <div className="shareOption flex items-center gap-2">
+              <Label htmlColor="blue" />
               <span className="shareOptionText">Tag</span>
             </div>
-            <div className="shareOption">
-              <Room />
+            <div className="shareOption flex items-center gap-2">
+              <Room htmlColor="green" />
               <span className="shareOptionText">Location</span>
             </div>
-            <div className="shareOption">
-              <EmojiEmotions />
+            <div className="shareOption flex items-center gap-2">
+              <EmojiEmotions htmlColor="goldenrod" />
               <span className="shareOptionText">Feelings</span>
             </div>
           </div>
         </div>
+        <button
+          className="shareButton
+           bg-blue-500 py-2 px-4 rounded-md text-white font-semibold m-2"
+        >
+          Share
+        </button>
       </div>
     </div>
   );
