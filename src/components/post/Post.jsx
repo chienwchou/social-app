2a53b6268ed5cff66f833fbcc0ec0f3394a2e7ad
Chien Wei Chou <chien.w.chou@gmail.com>
2024-03-16 17:49:30 -0700

    style post icons

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -3,30 +3,34 @@ import "./post.css";
 
 export default function Post() {
   return (
-    <div class="post m-4 shadow-xl rounded-md">
-      <div class="postWrapper mx-4 my-8">
-        <div class="postTop flex justify-between items-center py-2">
-          <div class="postTopLeft flex items-center gap-4">
-            <img class="postProfileImg" src="/assets/person/1.jpg" alt="" />
-            <span class="postUsername font-bold">Safak Kocaoglu</span>
-            <span class="postDate text-sm">5 mins ago</span>
+    <div className="post m-4 shadow-xl rounded-md">
+      <div className="postWrapper mx-4 my-8">
+        <div className="postTop flex justify-between items-center py-2">
+          <div className="postTopLeft flex items-center gap-4">
+            <img className="postProfileImg" src="/assets/person/1.jpg" alt="" />
+            <span className="postUsername font-bold">Safak Kocaoglu</span>
+            <span className="postDate text-sm">5 mins ago</span>
           </div>
-          <div class="postTopRight">
+          <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
-        <div class="postCenter pb-2">
-          <span class="postText">Hey! it's my first post</span>
-          <img class="postImg" src="/assets/post/1.jpeg" alt="" />
+        <div className="postCenter pb-2">
+          <span className="postText">Hey! it's my first post</span>
+          <img className="postImg" src="/assets/post/1.jpeg" alt="" />
         </div>
-        <div class="postBottom flex pb-4 justify-between">
-          <div class="postBottomLeft flex">
-            <img class="likeIcon" src="/assets/like.png" alt="" />
-            <img class="heartIcon" src="/assets/heart.png" alt="" />
-            <span class="postLikeCounter font-bold">32 people liked it</span>
+        <div className="postBottom flex pb-4 justify-between">
+          <div className="postBottomLeft flex items-center gap-2">
+            <div className="flex gap-1">
+              <img className="likeIcon" src="/assets/like.png" alt="" />
+              <img className="heartIcon" src="/assets/heart.png" alt="" />
+            </div>
+            <span className="postLikeCounter font-bold">
+              32 people liked it
+            </span>
           </div>
-          <div class="postBottomRight">
-            <span class="postCommentText">9 comments</span>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments</span>
           </div>
         </div>
       </div>
