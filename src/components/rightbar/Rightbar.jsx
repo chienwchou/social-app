da36b273b51e1b8fc77c0186d60b92b2fe4b9667
Chien Wei Chou <chien.w.chou@gmail.com>
2024-03-20 19:37:51 -0700

    finish rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,17 +4,21 @@ export default function Rightbar({ className, ...props }) {
   return (
     <div className={"rightbar " + className}>
       <div className="rightbarWrapper">
-        <div className="birthdayContainer">
+        <div className="birthdayContainer flex items-center gap-4 my-4">
           <img className="birthdayImg" src="/assets/gift.png" alt="" />
           <span className="birthdayText">
             <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.
           </span>
         </div>
-        <img className="rightbarAd" src="/assets/ad.png" alt="" />
-        <h4 className="rightbarTitle">Online Friends</h4>
+        <img
+          className="rightbarAd mb-4 p-4 rounded-3xl overflow-hidden"
+          src="/assets/ad.png"
+          alt=""
+        />
+        <h4 className="rightbarTitle font-bold mb-2">Online Friends</h4>
         <ul className="rightbarFriendList">
-          <li className="rightbarFriend">
-            <div className="rightbarProfileImgContainer">
+          <li className="rightbarFriend flex items-center gap-3">
+            <div className="rightbarProfileImgContainer relative">
               <img
                 className="rightbarProfileImg"
                 src="/assets/person/1.jpg"
@@ -22,6 +26,7 @@ export default function Rightbar({ className, ...props }) {
               />
               <span className="rightbarOnline"></span>
             </div>
+            <span className="rightbarUsername font-bold">John Carter</span>
           </li>
         </ul>
       </div>
